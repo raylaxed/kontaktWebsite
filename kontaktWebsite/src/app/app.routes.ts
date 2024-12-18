8d52b374955ac480cb75abe713bc45a1880f31b3
raylaxed <r.kohlprath@gmail.com>
2024-12-18 12:11:48 +0100

    animation show off

--- kontaktWebsite/src/app/app.routes.ts
+++ kontaktWebsite/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AnimationManagementComponent } from './component/animation-management/animation-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'animation', 
+    component: AnimationManagementComponent 
+  },
+  // Add a default route if needed
+  { 
+    path: '', 
+    redirectTo: '/animation', 
+    pathMatch: 'full' 
+  }
+];
