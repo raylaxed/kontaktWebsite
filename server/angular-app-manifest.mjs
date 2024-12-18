87ddeb0f75200c97a339c79125fed20176e59dcc
raylaxed <r.kohlprath@gmail.com>
2024-12-18 12:36:42 +0100

    deploy 1

---
+++ server/angular-app-manifest.mjs
@@ -0,0 +1,13 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: undefined,
+  assets: {
+    'index.csr.html': {size: 498, hash: 'd87a3b7a8e3a97c3c7400456ec26583b656c5a293c23d75face08e539e99928f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1011, hash: 'a5f82fbb931417dfef7412c7c440d5b76029b55327dc5ef99d248c352987db40', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+  },
+};
